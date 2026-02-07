4d6342cd2f1bbdfc496884a51cf7ce7c96214f87
Sumit <sumitbaghel22a@gmail.com>
2026-02-07 15:09:14 +0530

    cors policy fixed

---
+++ middleware.js
@@ -0,0 +1,11 @@
+import { NextResponse } from "next/server"
+
+export function middleware(req) {
+  const res = NextResponse.next()
+
+  res.headers.set("Access-Control-Allow-Origin", "*")
+  res.headers.set("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS")
+  res.headers.set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+
+  return res
+}
