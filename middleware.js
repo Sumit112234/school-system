f468ef68fb77c59b4f693acfe89197efc5b37c5b
Sumit <sumitbaghel22a@gmail.com>
2026-02-07 15:16:13 +0530

    cors policy fixed with restriction

--- middleware.js
+++ middleware.js
@@ -1,11 +1,29 @@
 import { NextResponse } from "next/server"
 
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://cgs-school-os-frontend.vercel.app"
+]
+
 export function middleware(req) {
+  const origin = req.headers.get("origin")
   const res = NextResponse.next()
 
-  res.headers.set("Access-Control-Allow-Origin", "*")
+  if (allowedOrigins.includes(origin)) {
+    res.headers.set("Access-Control-Allow-Origin", origin)
+  }
+
   res.headers.set("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS")
-  res.headers.set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+  res.headers.set(
+    "Access-Control-Allow-Headers",
+    "Content-Type, Authorization"
+  )
+  res.headers.set("Access-Control-Allow-Credentials", "true")
+
+  // Handle preflight
+  if (req.method === "OPTIONS") {
+    return new NextResponse(null, { headers: res.headers })
+  }
 
   return res
 }
